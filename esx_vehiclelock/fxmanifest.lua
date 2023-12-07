d1d5409b21f7bd927cc268e9b7cdfa80a921ad3b
tekken <74188150+tekkenkkk@users.noreply.github.com>
2023-12-07 13:16:39 +0100

    code cleanup + esx updated import

--- esx_vehiclelock/fxmanifest.lua
+++ esx_vehiclelock/fxmanifest.lua
@@ -1,28 +1,25 @@
 fx_version 'adamant'
-lua54        'yes'
+lua54 'yes'
 game 'gta5'
 
 description 'OX ESX Vehicle Lock'
 
 version '1.0.0'
 
+shared_scripts {
+	'@es_extended/imports.lua',
+	'@ox_lib/init.lua'
+}
+
 server_script {
-	'@mysql-async/lib/MySQL.lua',
-	'config.lua',
+	'@oxmysql/lib/MySQL.lua'
 	'server/main.lua'
 }
 
-client_scripts {
-	'config.lua',
-	'client/main.lua'
-}
+client_script 'client/main.lua'
 
 dependencies {
 	'es_extended',
-        'ox_lib',
+    'ox_lib',
 	'esx_vehicleshop'
-}
-
-shared_scripts {
-	'@ox_lib/init.lua',
 }
